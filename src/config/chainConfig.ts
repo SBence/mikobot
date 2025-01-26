4b0a15759edc77081cb27da69f620f790f44a3f2
SBence <43538202+SBence@users.noreply.github.com>
2025-01-26 18:21:44 +0100

    Clean, format and refactor code and configs
    - Format chainConfig.ts and tsconfig.json with Prettier
    - Make package.json scripts package manager agnostic
    - Remove .prettierignore
    - Use shared Node.js setup action in build workflow

--- src/config/chainConfig.ts
+++ src/config/chainConfig.ts
@@ -2,15 +2,12 @@ import { MarkovGenerateOptions, MarkovResult } from "markov-strings";
 
 const chainConfig: MarkovGenerateOptions = {
   maxTries: 4096,
-  filter: (result: MarkovResult) => {
-    return (
-      result.score >= 7 &&
-      !result.string.includes("@") &&
-      result.string.length <= 2000 &&
-      !result.string.includes("||") &&
-      !result.string.includes("http")
-    );
-  },
+  filter: (result: MarkovResult) =>
+    result.score >= 7 &&
+    !result.string.includes("@") &&
+    result.string.length <= 2000 &&
+    !result.string.includes("||") &&
+    !result.string.includes("http"),
 };
 
 export default chainConfig;
